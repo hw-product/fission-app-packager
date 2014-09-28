23fc82afcd541b2e6363760b8c548c2283a8f617
Chris Roberts <chrisroberts.code@gmail.com>
2014-09-28 06:16:54 -0700

    Isolate based on account

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :packager do
-    resources :repositories
-    resources :jobs
+    resources :accounts, :only => [] do
+      resources :repositories
+      resources :jobs
+    end
   end
 end
