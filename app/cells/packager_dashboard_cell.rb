7ec2a355b0861e292d08cb7f1543775c6c907e6d
Anthony Goddard <anthony@anthonygoddard.com>
2014-12-02 21:49:36 -0700

    placeholder dashboard widget for packager

--- app/cells/packager_dashboard_cell.rb
+++ app/cells/packager_dashboard_cell.rb
@@ -1,6 +1,8 @@
 class PackagerDashboardCell < Cell::Rails
 
   def show(args)
+    @jobs_summary = {:in_progress => 5, :error => 2, :complete => 7}
+    @popular = {:geminabox => 134, :httpd => 12, :nginx => 99}
     render
   end
 
