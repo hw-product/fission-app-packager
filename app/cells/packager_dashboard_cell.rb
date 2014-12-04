2156f0ec7b9ceb9ab4eb5283963e36f6bcc35923
Anthony Goddard <anthony@anthonygoddard.com>
2014-12-03 17:19:20 -0700

    get some real data for the cell

--- app/cells/packager_dashboard_cell.rb
+++ app/cells/packager_dashboard_cell.rb
@@ -1,8 +1,23 @@
-class PackagerDashboardCell < Cell::Rails
+class PackagerDashboardCell < DashboardCell
 
   def show(args)
-    @jobs_summary = {:in_progress => 5, :error => 2, :complete => 7}
-    @popular = {:geminabox => 134, :httpd => 12, :nginx => 99}
+    super
+    dataset = Job.dataset_with(:scalars => {:status => ['status']})
+    @jobs_summary = {
+      :in_progress => dataset.where(:status => 'active').count,
+      :error => dataset.where(:status => 'error').count,
+      :complete => dataset.where(:status => 'complete').count
+    }
+
+    repos = current_user.run_state.current_account.repositories_dataset.all.map{|repo|repo.name}
+    counts = repos.map do |repo|
+      github(:bot).releases(repo).map do |release|
+        release.assets.map do |asset|
+          {:name => "#{repo}/#{release.name}", :count => asset.download_count}
+        end
+      end
+    end.flatten
+    @popular = counts.sort_by { |k| k[:count] }.last(5).reverse
     render
   end
 
